6775920328d1caa65c73649099b2a3eac4da2414
Tommy Vinyard <tpvinyard@gmail.com>
2019-12-21 12:36:27 -0600

    Login.js & Register.js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, it's me</h1>
-    </div>
-  );
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,7 +7,7 @@ class Landing extends Component {
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
+            {/* <h4>
               <b>Build</b> a login/auth app with the{" "}
               <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
               scratch
@@ -15,7 +15,7 @@ class Landing extends Component {
             <p className="flow-text grey-text text-darken-1">
               Create a (minimal) full-stack app with user authentication via
               passport and JWTs
-            </p>
+            </p> */}
             <br />
             <div className="col s6">
               <Link
