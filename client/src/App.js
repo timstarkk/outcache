37b8151b1621c5f88ca8bca7eaf5a8b7861aa400
timstarkk <timstarkk@icloud.com>
2020-01-09 00:55:54 -0600

    removed results and search from search folder and removed import and call from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 import Form from "./pages/Form/Form"
-import Search from "./components/search/Search"
+// import Search from "./components/search/Search"
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -53,7 +53,7 @@ class App extends Component {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/form" component={Form} />
-            <Route exact path="/Search" component={Search} />
+            {/* <Route exact path="/Search" component={Search} /> */}
 
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
