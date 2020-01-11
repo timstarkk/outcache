d9d7b0b96f0f24ec1c888f0a983c825313fa1547
timstarkk <timstarkk@icloud.com>
2020-01-11 12:08:03 -0600

    added .get to /item route

--- routes/api/item.js
+++ routes/api/item.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 const itemsController = require("../../controllers/itemsController");
 
 
-  router.route("/item")
-  .post(itemsController.create);
+router.route("/item")
+  .post(itemsController.create)
+  .get(itemsController.findAll);
+
+// router.route("/:term")
+//   .get(itemsController.findByTerm);
+
+// router.route("/getAll")
+//   .get(itemsController.findAll);
 
 
 
