b07fa8a465427d4a221d0aefa21245737e4c5a8c
Tommy Vinyard <tpvinyard@gmail.com>
2020-02-25 10:06:46 -0600

    changing export default to module.exports

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -14,73 +14,74 @@ AWS.config.secretAccessKey = process.env.AWS_SECRET_KEY_ID;
 AWS.config.region = "us-east-1";
 
 // const sendEmail = async(userData) => {
-const sendEmail = async (userData) => {
-    return new Promise((resolve) => {
-        try {
-            let emailTemplate = fs.readFileSync(path.resolve(__dirname, '../email_templates/confirmation.html'), 'utf8');
-            let emailSubject = 'Outcache Rental Approved';
-            let sourceAddress = 'outcachetech@gmail.com';
+module.exports = { 
+    sendEmail = async (userData) => {
+        return new Promise((resolve) => {
+            try {
+                let emailTemplate = fs.readFileSync(path.resolve(__dirname, '../email_templates/confirmation.html'), 'utf8');
+                let emailSubject = 'Outcache Rental Approved';
+                let sourceAddress = 'outcachetech@gmail.com';
 
-            console.log(userData);
+                console.log(userData);
 
-            // Prepare data for template placeholders
-            let emailData = {
-                name: userData.body.firstName,
-                itemName: userData.body.itemName,
-                beginDate: userData.body.beginDate,
-                endDate: userData.body.endDate
-            };
+                // Prepare data for template placeholders
+                let emailData = {
+                    name: userData.body.firstName,
+                    itemName: userData.body.itemName,
+                    beginDate: userData.body.beginDate,
+                    endDate: userData.body.endDate
+                };
 
-            let templateHtml = handlebars.compile(emailTemplate.toString());
-            let bodyHtml = templateHtml(emailData);
+                let templateHtml = handlebars.compile(emailTemplate.toString());
+                let bodyHtml = templateHtml(emailData);
 
-            let params = {
-                Destination: {
-                    ToAddresses: [
-                        userData.body.emailAddress
-                    ]
-                },
-                Message: {
-                    Body: {
-                        Html: {
-                            Charset: 'UTF-8',
-                            Data: bodyHtml
+                let params = {
+                    Destination: {
+                        ToAddresses: [
+                            userData.body.emailAddress
+                        ]
+                    },
+                    Message: {
+                        Body: {
+                            Html: {
+                                Charset: 'UTF-8',
+                                Data: bodyHtml
+                            },
+                            Text: {
+                                Charset: 'UTF-8',
+                                Data: emailSubject
+                            }
                         },
-                        Text: {
+                        Subject: {
                             Charset: 'UTF-8',
                             Data: emailSubject
                         }
                     },
-                    Subject: {
-                        Charset: 'UTF-8',
-                        Data: emailSubject
-                    }
-                },
-                Source: sourceAddress,
-                ReplyToAddresses: [
-                    sourceAddress
-                ]
-            };
+                    Source: sourceAddress,
+                    ReplyToAddresses: [
+                        sourceAddress
+                    ]
+                };
 
-            let sendPromise = new AWS.SES({apiVersion: '2010-12-01'})
-                .sendEmail(params)
-                .promise();
+                let sendPromise = new AWS.SES({apiVersion: '2010-12-01'})
+                    .sendEmail(params)
+                    .promise();
 
-            sendPromise.then(
-                function(data) {
-                    //  data.MessageId
-                    console.log(data.MessageId);
-                    resolve(true);
-                }).catch(
-                function(err) {
-                    console.error(err, err.stack);
-                    resolve(false);
-                });
-        } catch (error) {
-            resolve(false);
-            console.log(error);
-        }
-    });
+                sendPromise.then(
+                    function(data) {
+                        //  data.MessageId
+                        console.log(data.MessageId);
+                        resolve(true);
+                    }).catch(
+                    function(err) {
+                        console.error(err, err.stack);
+                        resolve(false);
+                    });
+            } catch (error) {
+                resolve(false);
+                console.log(error);
+            }
+        });
+    }
 };
 
-export default sendEmail;
