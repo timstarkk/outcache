eff96c11a1381c25c1902eb42516bb04fbbd0ac2
timstarkk <timstarkk@icloud.com>
2020-03-13 02:23:44 -0500

    changed rental card styling to match resultcard

--- client/src/components/Dashboard/RentalCard.js
+++ client/src/components/Dashboard/RentalCard.js
@@ -18,9 +18,10 @@ function RentalCard(props) {
                 <div className="cardInfoContainer">
                     <h6 className="cardName">{name}</h6>
                     <p className="cardLocation" style={{ padding: "0px", margin: "0px" }}>{location}</p>
-                    <div className="row">
-                        <p className="col s3 cardPrice" style={{ padding: "0px", "padding-left": "15px" }}>${price}</p>
-                        <p className="col s3 perDayText" style={{ padding: "0px" }}>{' '}/ day</p>
+                    <div className="row" style={{ marginBottom: "0px", width: "100%" }}>
+                        <p className="cardPrice" style={{ padding: "0px", "padding-left": "11px" }}>${price}</p>
+                        <span> </span>
+                        <p className="perDayText" style={{ padding: "0px" }}>{' '}/ day</p>
                     </div>
                 </div>
             </div >
